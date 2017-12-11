c90c6584307afaf0bd491051aaeb18261f571549
luisaGonzales <luisagc711@gmail.com>
2017-12-11 13:18:32 -0500

    First component

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,8 +1,7 @@
 import createStore from 'redux-zero';
 
 const initialState = {
-    gender: ["male", "female"], 
-    age: ["adult", "children", "baby"], 
+    title: "Create it"
 };
 
 const store = createStore(initialState);
